1a0eb08e0474847d25c7b3ab2a7dbc9a92284043
PuReWebDev <purewebdev@gmail.com>
2023-04-06 18:13:22 -0400

    create tokens table migration

---
+++ database/migrations/2023_04_06_215637_create_tokens_table.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTokensTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tokens', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->text('code')->unique();
+            $table->text('token')->nullable()->unique();
+            $table->text('refresh_token')->nullable()->unique();
+            $table->integer('expires_in')->nullable();
+            $table->integer('refresh_token_expires_in')->nullable();
+            $table->text('abilities')->nullable();
+            $table->timestamp('last_used_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tokens');
+    }
+}
