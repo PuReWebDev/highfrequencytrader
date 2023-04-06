0025a0ec19d47d4cb7d318706909ea7b21d0201e
PuReWebDev <purewebdev@gmail.com>
2023-04-06 17:38:56 -0400

    table to store market hours, session open/close information

--- database/migrations/2023_01_09_025255_create_markets_table.php
+++ database/migrations/2023_01_09_025255_create_markets_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateMarketsTable extends Migration
+class CreateMarketHoursTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,18 @@ class CreateMarketsTable extends Migration
      */
     public function up()
     {
-        Schema::create('markets', function (Blueprint $table) {
+        Schema::create('market_hours', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
+            $table->string('category')->nullable();
+            $table->string('date');
+            $table->string('exchange');
+            $table->boolean('is_open');
+            $table->string('market_type');
+            $table->string('product')->nullable();
+            $table->string('product_name')->nullable();
             $table->boolean('is_open')->default(false);
+            $table->string('session_hours');
+            $table->dateTime('date');
             $table->dateTime('open_time');
             $table->dateTime('close_time');
             $table->timestamps();
@@ -30,6 +38,6 @@ class CreateMarketsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('markets');
+        Schema::dropIfExists('market_hours');
     }
 }
