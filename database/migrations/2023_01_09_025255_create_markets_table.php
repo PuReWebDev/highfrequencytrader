c1dde4a09f8e7c8f9c3d8cea8c36965cc5cd3452
PuReWebDev <purewebdev@gmail.com>
2023-04-06 18:16:09 -0400

    create market_hours table

--- database/migrations/2023_01_09_025255_create_markets_table.php
+++ database/migrations/2023_01_09_025255_create_markets_table.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -16,15 +18,13 @@ class CreateMarketHoursTable extends Migration
         Schema::create('market_hours', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('category')->nullable();
-            $table->string('date');
             $table->string('exchange');
             $table->boolean('is_open');
             $table->string('market_type');
             $table->string('product')->nullable();
             $table->string('product_name')->nullable();
             $table->boolean('is_open')->default(false);
-            $table->string('session_hours');
-            $table->dateTime('date');
+            $table->string('session_hours')->nullable();
             $table->dateTime('open_time');
             $table->dateTime('close_time');
             $table->timestamps();
