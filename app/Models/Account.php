2ce155bda48c5206be51ad62926bd1169967eba4
PuReWebDev <purewebdev@gmail.com>
2023-04-09 19:55:59 -0400

    Account migration and model

---
+++ app/Models/Account.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Account extends Model
+{
+    use HasFactory;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'accounts';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'user_id',
+        'account_id',
+        'type',
+        'roundTrips',
+        'isDayTrader',
+        'isClosingOnlyRestricted',
+    ];
+
+    /**
+     * Get the user that owns the phone.
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Get the Orders associated with the user.
+     */
+    public function orders(): HasOne
+    {
+        return $this->hasMany(Order::class);
+    }
+
+    /**
+     * Get the Posiions associated with the user.
+     */
+    public function positions(): HasOne
+    {
+        return $this->hasMany(Position::class);
+    }
+}
---
+++ database/migrations/2023_04_09_223649_create_accounts_table.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAccountsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('accounts', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('accountId');
+            $table->string('type');
+            $table->integer('roundTrips');
+            $table->boolean('isDayTrader');
+            $table->boolean('isClosingOnlyRestricted');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('accounts');
+    }
+}
