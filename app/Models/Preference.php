dcb6ca7cf891fcb26add4c37ab4de356801782fd
PuReWebDev <purewebdev@gmail.com>
2023-04-11 18:48:51 -0400

    Preference migration and model

---
+++ app/Models/Preference.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Preference extends Model
+{
+    use HasFactory;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'accounts';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'user_id',
+        'tradingEnabled',
+        'tradePreMarket',
+        'tradePostMarket',
+        'acceptTos',
+        'tradeQuantity',
+    ];
+
+    /**
+     * Get the user that owns the phone.
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
