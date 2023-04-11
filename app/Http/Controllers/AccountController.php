34bcb5d02475e169a6c118cf3193ffe8a108ac55
PuReWebDev <purewebdev@gmail.com>
2023-04-11 10:07:16 -0400

    logging

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -186,6 +186,7 @@ class AccountController extends Controller
      */
     private static function saveOrdersInformation($orderStrategies): void
     {
+        Log::debug('Orders: ', $orderStrategies);
         Order::updateOrCreate(
             [
                 'user_id' => Auth::id(),
