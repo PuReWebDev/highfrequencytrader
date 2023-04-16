febc44005bd8656f4bd1bd4a88dceabdb7b10855
PuReWebDev <purewebdev@gmail.com>
2023-04-16 15:07:08 -0400

    back to new new

--- app/Services/OrderService.php
+++ app/Services/OrderService.php
@@ -202,8 +202,8 @@ class OrderService
         $account = Account::where('user_id', Auth::id())->get();
         $ordersEndpointUrl = config('tdameritrade.base_url') . '/v1/accounts/' . $account['0']['accountId'] . '/orders';
 
-//        return self::sendRequest($ordersEndpointUrl, $newnew);
-        return self::sendRequest($ordersEndpointUrl, $theChildren);
+        return self::sendRequest($ordersEndpointUrl, $newnew);
+//        return self::sendRequest($ordersEndpointUrl, $theChildren);
     }
 
     /**
