47e0b8cb94a53f230566020e565a71f530cf2fc3
PuReWebDev <purewebdev@gmail.com>
2023-04-20 09:51:31 -0400

    buy and sell

--- app/Services/OrderService.php
+++ app/Services/OrderService.php
@@ -145,8 +145,8 @@ class OrderService
         $account = Account::where('user_id', Auth::id())->get();
         $ordersEndpointUrl = config('tdameritrade.base_url') . '/v1/accounts/' . $account['0']['accountId'] . '/orders';
 
-//        return self::sendRequest($ordersEndpointUrl, $newnew);
-        return self::sendRequest($ordersEndpointUrl, $sellOut);
+        return self::sendRequest($ordersEndpointUrl, $newnew);
+//        return self::sendRequest($ordersEndpointUrl, $sellOut);
     }
 
     /**
