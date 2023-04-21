216277fdb7adef1e81549f65db66d0c26cfa814d
PuReWebDev <purewebdev@gmail.com>
2023-04-21 14:29:13 -0400

    fetch account from the Order controller.

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -5,9 +5,12 @@ declare(strict_types=1);
 namespace App\Http\Controllers;
 
 use App\Models\Order;
+use App\Models\Token;
+use App\TDAmeritrade\Accounts;
 use App\TDAmeritrade\TDAmeritrade;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\View;
 
 class OrderController extends Controller
@@ -19,6 +22,19 @@ class OrderController extends Controller
      */
     public function index()
     {
+        $token = Token::where('user_id', Auth::id())->get();
+
+        if (TDAmeritrade::isAccessTokenExpired
+            ($token['0']['updated_at']) === true) {
+            // Time To Refresh The Token
+            TDAmeritrade::saveTokenInformation(TDAmeritrade::refreshToken($token['0']['refresh_token']));
+            Log::info('The Token Was Refreshed During This Process');
+        }
+
+        // Retrieve The Account Information
+        $accountResponse = Accounts::getAccounts();
+        Accounts::saveAccountInformation($accountResponse);
+
         $orders = Order::where('user_id', Auth::id())->orderBy('enteredTime', 'DESC')->get();
         list($workingCount, $filledCount, $rejectedCount, $cancelledCount,
             $expiredCount) = TDAmeritrade::extracted($orders);
