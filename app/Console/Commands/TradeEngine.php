873998441bbfcd733ae8832bcd2b62c355194846
PuReWebDev <purewebdev@gmail.com>
2023-04-23 16:44:28 -0400

    count preference

--- app/Console/Commands/TradeEngine.php
+++ app/Console/Commands/TradeEngine.php
@@ -83,7 +83,7 @@ class TradeEngine extends Command
             }
             // If all orders have completed, place a new OTO order
             if ($orders->count() <= $tradeQuantity) {
-                $this->info('We have '. count($orders) .' . $consecutiveTrades is: '. $consecutiveTrades . ' and $sharesPerTrades is:'. $sharesPerTrade);
+                $this->info('We have '. $orders->count() .' working orders. $consecutiveTrades is: '. $consecutiveTrades . ' and $sharesPerTrades is:'. $sharesPerTrade);
                 // Grab The Current Price
                 $quotes = TDAmeritrade::quotes([$symbol,'AMZN']);
 
