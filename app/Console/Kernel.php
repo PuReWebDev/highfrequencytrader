4891f2efc2fe12ccc8e5dd863806095b42b1f77b
PuReWebDev <purewebdev@gmail.com>
2023-04-23 17:16:41 -0400

    schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,6 +16,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->command('trader TSLA')->dailyAt('07:00');
     }
 
     /**
