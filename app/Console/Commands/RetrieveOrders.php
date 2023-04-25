833896b9d89b8ad325e98bc02060274624c622a3
PuReWebDev <purewebdev@gmail.com>
2023-04-25 16:28:49 -0400

    fetch all statuses

--- app/Console/Commands/RetrieveOrders.php
+++ app/Console/Commands/RetrieveOrders.php
@@ -4,10 +4,12 @@ declare(strict_types=1);
 
 namespace App\Console\Commands;
 
+use App\Models\Order;
 use App\TDAmeritrade\TDAmeritrade;
 use Carbon\Carbon;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 
 class RetrieveOrders extends Command
 {
@@ -47,10 +49,29 @@ class RetrieveOrders extends Command
 
         Auth::loginUsingId(4, $remember = true);
 
+        if ($status === strtoupper('all')) {
+            $status = '';
+        }
+
         while (true) {
             TDAmeritrade::getOrders($status);
             $this->info($status.' Orders Retrieved. '.Carbon::now());
             usleep(500000);
+
+            // Splitting The Workload for basic clean up of stale orders
+            $pendingCancels = Order::where([
+                ['user_id', '=', Auth::id()],
+                ['tag', '=', 'AA_PuReWebDev'],
+                ['instruction', '=', 'BUY'],
+                ['created_at', '<=', Carbon::now()->subMinutes(2)
+                    ->toDateTimeString()],
+            ])->whereIn('status',['WORKING','PENDING_ACTIVATION'])->get();
+
+            foreach ($pendingCancels as $pendingCancel) {
+                TDAmeritrade::cancelOrder($pendingCancel['orderId']);
+                Log::info('Stale Buy Order Cancelled: '.$pendingCancel['orderId']);
+                $this->info('Stale Buy Order Cancelled: '.$pendingCancel['orderId']);
+            }
         }
 
         $this->info('Trade Orders Retrieval Gracefully Exiting');
