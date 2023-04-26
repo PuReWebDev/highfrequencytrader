46aeb9ed13a6b50be9bc84825b32b6dfb3bf556c
PuReWebDev <purewebdev@gmail.com>
2023-04-26 14:56:38 -0400

    symbol

--- app/Console/Commands/TradeEngine.php
+++ app/Console/Commands/TradeEngine.php
@@ -29,7 +29,7 @@ class TradeEngine extends Command
      */
     protected $description = 'Initiate Trade Engine For Client';
 
-    protected array $tradeSymbols = ['TSLA','AAPL',];
+    protected array $tradeSymbols = ['TSLA',];
 
     /**
      * Create a new command instance.
