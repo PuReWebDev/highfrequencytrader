b37504704602f908fd7af124ceee550c5bfc6037
PuReWebDev <purewebdev@gmail.com>
2023-04-26 11:28:57 -0400

    adjusting trade workers again

--- app/Console/Commands/TradeEngine.php
+++ app/Console/Commands/TradeEngine.php
@@ -50,7 +50,7 @@ class TradeEngine extends Command
     {
         // Get stock symbol from command argument
         $symbol = $this->argument('symbol');
-        $tradeQuantity = 70;
+        $tradeQuantity = 100;
         $sharesPerTrade = 2;
         $consecutiveTrades = 0;
 
