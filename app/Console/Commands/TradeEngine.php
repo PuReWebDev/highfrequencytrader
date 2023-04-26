98c8e1929a0888766f650f45bb7ec935aaf04f60
PuReWebDev <purewebdev@gmail.com>
2023-04-26 13:22:56 -0400

    add sbux

--- app/Console/Commands/TradeEngine.php
+++ app/Console/Commands/TradeEngine.php
@@ -29,7 +29,7 @@ class TradeEngine extends Command
      */
     protected $description = 'Initiate Trade Engine For Client';
 
-    protected array $tradeSymbols = ['TSLA',];
+    protected array $tradeSymbols = ['TSLA','SBUX',];
 
     /**
      * Create a new command instance.
