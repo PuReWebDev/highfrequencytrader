10c38c9af08b4062ccc78b96928b4451e190f5bd
PuReWebDev <purewebdev@gmail.com>
2023-04-27 13:18:35 -0400

    restore same view for now

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -28,14 +28,15 @@ class OrderController extends Controller
 //        TDAmeritrade::getOrders('FILLED');
 //        Accounts::updateAccountData();
 
-        $yesterday = Carbon::yesterday();
-        $now = Carbon::now();
+//        $yesterday = Carbon::yesterday();
+//        $now = Carbon::now();
 
         $orders = Order::where([
             ['user_id','=', Auth::id()],
             ['tag', '=', 'AA_PuReWebDev'],
         ])->whereNotNull('instruction')->whereNotNull('positionEffect')
-            ->whereBetween('created_at', [$yesterday, $now])->orderBy('orderId', 'DESC')->get();
+//            ->whereBetween('created_at', [$yesterday, $now])->orderBy('orderId', 'DESC')->get();
+            ->whereDate('created_at', Carbon::today())->orderBy('orderId', 'DESC')->get();
 
         $orders->each(function ($item, $key) {
             // Readable time vs raw timestamp
