7470f3712de6a605ee22c2de566ea41485ca0f8a
PuReWebDev <purewebdev@gmail.com>
2023-04-27 08:47:57 -0400

    adjust to see working orders

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -28,9 +28,15 @@ class OrderController extends Controller
 //        TDAmeritrade::getOrders('FILLED');
 //        Accounts::updateAccountData();
 
+//        $orders = Order::where([
+//            ['user_id','=', Auth::id()],
+//            ['tag', '=', 'AA_PuReWebDev'],
+//        ])->whereNotNull('instruction')->whereNotNull('positionEffect')->whereDate('created_at', Carbon::today())->orderBy('orderId', 'DESC')->get();
+
         $orders = Order::where([
             ['user_id','=', Auth::id()],
             ['tag', '=', 'AA_PuReWebDev'],
+            ['status', '=', 'WORKING'],
         ])->whereNotNull('instruction')->whereNotNull('positionEffect')->whereDate('created_at', Carbon::today())->orderBy('orderId', 'DESC')->get();
 
         $orders->each(function ($item, $key) {
