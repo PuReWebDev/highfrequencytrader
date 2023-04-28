8a7b49b395a5326f46859db3cc23f97645f0aba8
PuReWebDev <purewebdev@gmail.com>
2023-04-27 20:10:05 -0400

    populate the actualProfit column

--- app/TDAmeritrade/Accounts.php
+++ app/TDAmeritrade/Accounts.php
@@ -675,6 +675,7 @@ class Accounts
                 'requestedDestination' => $orderStrategies['requestedDestination'] ?? null,
                 'destinationLinkName' => $orderStrategies['destinationLinkName'] ?? null,
                 'price' => $orderStrategies['price'] ?? null,
+                'actualProfit' => $orderStrategies['actualProfit'] ?? null,
                 'stopPrice' => $orderStrategies['stopPrice'] ?? null,
                 'stopPriceLinkBasis' => $orderStrategies['stopPriceLinkBasis'] ?? null,
                 'stopPriceLinkType' => $orderStrategies['stopPriceLinkType'] ?? null,
