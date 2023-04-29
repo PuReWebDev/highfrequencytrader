64f6f32a16f834a94a0d90af051c5ab0f9e633a6
PuReWebDev <purewebdev@gmail.com>
2023-04-29 19:37:15 -0400

    updating class

--- app/TDAmeritrade/BaseClass.php
+++ app/TDAmeritrade/BaseClass.php
@@ -4,45 +4,47 @@ declare(strict_types=1);
 
 namespace App\TDAmeritrade;
 
+use App\Models\Token;
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
+use Illuminate\Support\Facades\Auth;
 
 abstract class BaseClass
 {
     /**
      * The HTTP client instance.
      *
-     * @var \GuzzleHttp\Client
+     * @var Client
      */
-    protected $client;
+    protected Client $client;
 
     /**
      * The TD Ameritrade API key.
      *
      * @var string
      */
-    protected $apiKey;
+    protected mixed $apiKey;
 
     /**
      * Create a new BaseClass instance.
      *
-     * @param  \GuzzleHttp\Client  $client
-     * @param  string  $apiKey
-     * @return void
      */
     public function __construct()
     {
         $this->client = new Client([
             'base_uri' => "https://api.tdameritrade.com",
         ]);
-        $this->apiKey = config('tdameritrade.api_key');
+        $token = Token::where('user_id', Auth::id())->get();
+        $this->apiKey = $token['0']['access_token'];
     }
 
     /**
      * Make a GET request to the TD Ameritrade API.
      *
-     * @param  string  $uri
-     * @param  array  $query
+     * @param string $uri
+     * @param array $query
      * @return array
+     * @throws \JsonException|GuzzleException
      */
     public function get(string $uri, array $query = []): array
     {
@@ -59,9 +61,10 @@ abstract class BaseClass
     /**
      * Make a POST request to the TD Ameritrade API.
      *
-     * @param  string  $uri
-     * @param  array  $data
+     * @param string $uri
+     * @param array $data
      * @return array
+     * @throws \JsonException|GuzzleException
      */
     protected function post(string $uri, array $data = []): array
     {
@@ -78,9 +81,10 @@ abstract class BaseClass
     /**
      * Make a PUT request to the TD Ameritrade API.
      *
-     * @param  string  $path
-     * @param  array  $data
+     * @param string $path
+     * @param array $data
      * @return array
+     * @throws \JsonException
      */
     protected function put(string $path, array $data): array
     {
@@ -89,14 +93,15 @@ abstract class BaseClass
             'json' => $data,
         ]);
 
-        return json_decode((string) $response->getBody(), true);
+        return json_decode((string)$response->getBody(), true, 512, JSON_THROW_ON_ERROR);
     }
 
     /**
      * Make a DELETE request to the TD Ameritrade API.
      *
-     * @param  string  $path
+     * @param string $path
      * @return array
+     * @throws \JsonException|GuzzleException
      */
     protected function delete(string $path): array
     {
@@ -104,7 +109,7 @@ abstract class BaseClass
             'headers' => $this->headers,
         ]);
 
-        return json_decode((string) $response->getBody(), true);
+        return json_decode((string)$response->getBody(), true, 512, JSON_THROW_ON_ERROR);
     }
 
 }
