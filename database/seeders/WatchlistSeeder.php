20460565f02e3344d854dfe86ddd6fe0686ef008
PuReWebDev <purewebdev@gmail.com>
2023-04-30 16:39:52 -0400

    new seeder for the watchlist table

---
+++ database/seeders/WatchlistSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Database\Seeders;
+
+use App\Models\WatchList;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Auth;
+
+class WatchlistSeeder extends Seeder
+{
+    protected array $tradeSymbols = ['TSLA', 'MSFT', 'GOOGL','BA', 'CRM', 'ABNB', 'DASH', 'UBER', 'AAPL', 'GD', 'NVDA', 'DIS', 'BIDU', 'UPS', 'SBUX', 'MCD', 'MMM', 'CSCO', 'CVS', 'WM', 'V', 'NFLX', 'SPG', 'FDX', 'LOW'];
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Auth::loginUsingId(4, $remember = true);
+
+        foreach ($this->tradeSymbols as $tradeSymbol) {
+            WatchList::updateOrCreate([
+                'user_id' => Auth::id(),
+                'symbol' => $tradeSymbol
+            ],[
+                'user_id' => Auth::id(),
+                'symbol' => $tradeSymbol,
+                'enabled' => true,
+            ]);
+        }
+    }
+}
