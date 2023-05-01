1eae8aec244dc91483480c6f038a9f9bbb0bc220
PuReWebDev <purewebdev@gmail.com>
2023-05-01 16:51:21 -0400

    dispatch orders only if working orders updated

--- app/Console/Commands/RetrieveOrders.php
+++ app/Console/Commands/RetrieveOrders.php
@@ -65,11 +65,12 @@ class RetrieveOrders extends Command
 
             if ($status === 'WORKING') {
                 $this->cancelStaleOrders();
-            }
 
-            $this->info('Dispatching To Trade Engine Processor '.Carbon::now());
-            OrdersProcessed::dispatch();
-            $this->info('Trade Engine Processor Completed'.Carbon::now());
+
+                $this->info('Dispatching To Trade Engine Processor '.Carbon::now());
+                OrdersProcessed::dispatch();
+                $this->info('Trade Engine Processor Completed'.Carbon::now());
+            }
         }
 
         $this->info('Trade Orders Retrieval Gracefully Exiting');
