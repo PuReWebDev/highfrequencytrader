8af8b62365ede78336b9b8b750484607debd7fce
PuReWebDev <purewebdev@gmail.com>
2023-05-01 15:21:33 -0400

    time between cancels

--- app/Console/Commands/RetrieveOrders.php
+++ app/Console/Commands/RetrieveOrders.php
@@ -87,7 +87,7 @@ class RetrieveOrders extends Command
             ['user_id', '=', Auth::id()],
             ['tag', '=', 'AA_PuReWebDev'],
             ['instruction', '=', 'BUY'],
-            ['created_at', '<=', Carbon::now()->subMinutes(2)
+            ['created_at', '<=', Carbon::now()->subMinutes(3)
                 ->toDateTimeString()],
         ])->whereIn('status', ['WORKING', 'PENDING_ACTIVATION'])->get();
 
