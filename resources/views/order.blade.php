e06e3a3e7d6f94c4006e6b4141891ce09ce445b4
PuReWebDev <purewebdev@gmail.com>
2023-05-03 17:02:31 -0400

    add date form

--- resources/views/order.blade.php
+++ resources/views/order.blade.php
@@ -10,6 +10,16 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
 
+                    <h2 class="mb-4">Select A Date To View Orders From</h2>
+                    <div class="form-group">
+                        <div class='input-group date' id='datetimepicker'>
+                            <input type='text' class="form-control" />
+                            <span class="input-group-addon">
+              <span class="glyphicon glyphicon-calendar"></span>
+            </span>
+                        </div>
+                    </div>
+
                     @if (count($orders) >= 1)
 
                                 @foreach($orders as $order)
