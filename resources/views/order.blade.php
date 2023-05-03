1c3c99c411797bd6e160f4929e9370817901a0d2
PuReWebDev <purewebdev@gmail.com>
2023-05-03 16:26:15 -0400

    check the time

--- resources/views/order.blade.php
+++ resources/views/order.blade.php
@@ -210,7 +210,8 @@
                                                     ($order->enteredTime)
                                                     ->toDateTimeString() }}</td>
 {{--                                                    <td>{{ \Carbon\Carbon::parse($order->closeTime)->format('g:i:s a') }}</td>--}}
-                                                    <td>{{ \Carbon\Carbon::parse($order->closeTime)->format('g:i A')->toDateTimeString() }}</td>
+                                                    <td>{{ \Carbon\Carbon::parse($order->closeTime)->format('g:i A') }}</td>
+{{--                                                    <td>{{ \Carbon\Carbon::parse($order->closeTime)->toDateTimeString() }}</td>--}}
 
                                                     <td>{{ gmdate('H:i:s', \Carbon\Carbon::parse($order->closeTime)->diffInSeconds($order->enteredTime)) }}</td>
 {{--                                                    <td>{{ \Carbon\Carbon::parse($order->closeTime)->diffInSeconds($order->enteredTime)->format('g:i:s a') }}</td>--}}
