352f0a7b300c8f293d7d4ff70a22fbf1e02b3498
PuReWebDev <purewebdev@gmail.com>
2023-05-04 18:36:30 -0400

    testing movers

--- app/Http/Controllers/MoverController.php
+++ app/Http/Controllers/MoverController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Mover;
 use App\TDAmeritrade\TDAmeritrade;
 
 class MoverController extends Controller
@@ -12,6 +13,26 @@ class MoverController extends Controller
      */
     public function index()
     {
-        TDAmeritrade::getMovers('$COMPX');
+        $movers = TDAmeritrade::getMovers('$COMPX');
+        foreach ($movers as $mover) {
+            self::saveMovers($mover);
+        }
+
+        dd($movers);
+//        $movers = TDAmeritrade::getMovers('$SPX.X');
+    }
+
+    private static function saveMovers(array $data)
+    {
+        Mover::updateOrCreate([
+            'symbol' => $data['symbol'],
+        ],[
+            'change' => $data['change'],
+            'description' => $data['description'],
+            'direction' => $data['direction'],
+            'last' => $data['last'],
+            'symbol' => $data['symbol'],
+            'totalVolume' => $data['totalVolume'],
+        ]);
     }
 }
