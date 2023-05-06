763e16950ff16f6fb8b6e1a771dff86d89adc0fa
PuReWebDev <purewebdev@gmail.com>
2023-05-06 16:08:49 -0400

    new symbol blade

---
+++ resources/views/symbol.blade.php
@@ -0,0 +1,44 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Symbol') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+
+                    <div class="table-responsive">
+                        <table class="table table-striped caption-top"
+                               id="symbol-detail-table">
+                            <caption style="text-align: center;">Today's
+                                Symbol Detail
+                            </caption>
+                            <thead>
+                            <tr>
+                                <th scope="col" style="white-space: nowrap;">
+                                <th style="white-space: nowrap;">Symbol</th>
+                                <th style="white-space: nowrap;">Description</th>
+                                </th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td style="white-space:
+                                                    nowrap;">{{ $symbol->symbol }}</td>
+                                    <td style="white-space: nowrap;">{{ $symbol->description}}</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+</x-app-layout>
