20495b01c3faaf71163483fda71a569edeac3c72
PuReWebDev <purewebdev@gmail.com>
2023-05-06 17:49:27 -0400

    tab

--- resources/views/symbol.blade.php
+++ resources/views/symbol.blade.php
@@ -10,6 +10,45 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
 
+                    <ul class="nav nav-tabs" id="myTab" role="tablist">
+                        <li class="nav-item" role="presentation">
+                            <button class="nav-link active" id="summary-tab"
+                                    data-bs-toggle="tab"
+                                    data-bs-target="#summary-tab-pane"
+                                    type="button" role="tab"
+                                    aria-controls="summary-tab-pane"
+                                    aria-selected="true">Summary</button>
+                        </li>
+                        <li class="nav-item" role="presentation">
+                            <button class="nav-link" id="news-tab"
+                                    data-bs-toggle="tab"
+                                    data-bs-target="#news-tab-pane"
+                                    type="button" role="tab"
+                                    aria-controls="news-tab-pane"
+                                    aria-selected="false">News</button>
+                        </li>
+                        <li class="nav-item" role="presentation">
+                            <button class="nav-link" id="chart-tab"
+                                    data-bs-toggle="tab"
+                                    data-bs-target="#chart-tab-pane"
+                                    type="button" role="tab"
+                                    aria-controls="chart-tab-pane"
+                                    aria-selected="false">Charts</button>
+                        </li>
+                        <li class="nav-item" role="presentation">
+                            <button class="nav-link" id="disabled-tab" data-bs-toggle="tab" data-bs-target="#disabled-tab-pane" type="button" role="tab" aria-controls="disabled-tab-pane" aria-selected="false" disabled>Disabled</button>
+                        </li>
+                    </ul>
+                    <div class="tab-content" id="myTabContent">
+                        <div class="tab-pane fade show active" id="summary-tab-pane" role="tabpanel" aria-labelledby="summary-tab" tabindex="0">{{ $symbol['0']['Description']}}</div>
+                        <div class="tab-pane fade" id="news-tab-pane"
+                             role="tabpanel" aria-labelledby="news-tab"
+                             tabindex="0">{{ $symbol['0']['symbol'] }}
+                            News</div>
+                        <div class="tab-pane fade" id="chart-tab-pane" role="tabpanel" aria-labelledby="chart-tab" tabindex="0">...</div>
+                        <div class="tab-pane fade" id="disabled-tab-pane" role="tabpanel" aria-labelledby="disabled-tab" tabindex="0">...</div>
+                    </div>
+
                     <div class="table-responsive">
                         <table class="table table-striped caption-top"
                                id="symbol-detail-table">
