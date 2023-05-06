f8351ad83d86c73e22e18bed509f7298290f2844
PuReWebDev <purewebdev@gmail.com>
2023-05-06 14:41:50 -0400

    testing method call

--- app/Http/Controllers/SymbolController.php
+++ app/Http/Controllers/SymbolController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Symbol;
 use App\TDAmeritrade\TDAmeritrade;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class SymbolController extends Controller
