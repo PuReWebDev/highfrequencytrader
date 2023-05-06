e0ec0e6aaca6af066f2f4cc1d93718a4d3bfb687
PuReWebDev <purewebdev@gmail.com>
2023-05-06 16:34:44 -0400

    nowrap

--- resources/views/symbol.blade.php
+++ resources/views/symbol.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Symbol') }}
+            {{ $symbol['0']['symbol'] }} Details
         </h2>
     </x-slot>
 
