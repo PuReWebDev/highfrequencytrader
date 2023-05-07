18729b3ce0dbfde6d3dd06238fc800556c472d8e
PuReWebDev <purewebdev@gmail.com>
2023-05-06 21:49:40 -0400

    formatting the price

--- resources/views/symbol.blade.php
+++ resources/views/symbol.blade.php
@@ -223,6 +223,16 @@
     </div>
 
 <script type="text/javascript">
+    // Create our number formatter.
+    const formatter = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+
+        // These options are needed to round to whole numbers if that's what you want.
+        //minimumFractionDigits: 0, // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
+        //maximumFractionDigits: 0, // (causes 2500.99 to be printed as $2,501)
+    });
+
     var options = {
         series: [{
             data: [
@@ -260,7 +270,8 @@
             },
             labels: {
                 formatter: function (value) {
-                    return value + "$";
+                    // return "$" + value;
+                    return formatter.format(value);
                 }
             }
         }
