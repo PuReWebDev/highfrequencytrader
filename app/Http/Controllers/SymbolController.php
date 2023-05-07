6deefce787cba83816a5ea9d6af3a2b1f1c6ce69
PuReWebDev <purewebdev@gmail.com>
2023-05-07 10:58:13 -0400

    debugging chart time series

--- app/Http/Controllers/SymbolController.php
+++ app/Http/Controllers/SymbolController.php
@@ -33,7 +33,7 @@ class SymbolController extends Controller
             if ($dt->isWeekend() === true) {
                 return Price::where([
                     ['symbol', '=', $symbol],
-                    ['datetime', '>=', Carbon::parse('last Friday')->timestamp],
+                    ['datetime', '>=', Carbon::parse('Friday')->timestamp],
                 ])->get();
             }
             return Price::where([
