728b5b7dc64a70536eede920971f5f7983a9d0d1
PuReWebDev <purewebdev@gmail.com>
2023-05-07 12:47:54 -0400

    debug

--- app/Http/Controllers/SymbolController.php
+++ app/Http/Controllers/SymbolController.php
@@ -30,12 +30,12 @@ class SymbolController extends Controller
             ])->whereDate('created_at', Carbon::today())->get();
         } else {
             $dt = Carbon::now();
-//            if ($dt->isWeekend() === true) {
-//                return Price::where([
-//                    ['symbol', '=', $symbol],
-//                    ['datetime', '>=', Carbon::parse('last Friday')->setTimezone('America/New_York')->getPreciseTimestamp(3)],])->get();
-////                ])->whereDate('created_at', Carbon::today()->setTimezone('America/New_York'))->get();
-//            }
+            if ($dt->isWeekend() === true) {
+                return Price::where([
+                    ['symbol', '=', $symbol],
+                    ['datetime', '>=', Carbon::parse('last Friday')->setTimezone('America/New_York')->getPreciseTimestamp(3)],])->get();
+//                ])->whereDate('created_at', Carbon::today()->setTimezone('America/New_York'))->get();
+            }
             return Price::where([
                 ['symbol', '=', $symbol],
                 ['datetime', '>=', Carbon::today()->setTimezone('America/New_York')->getPreciseTimestamp(3)],])->get();
