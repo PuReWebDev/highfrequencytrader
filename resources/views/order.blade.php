584b4e8426e048fa4797a1fa2bb72866ee8ce72e
PuReWebDev <purewebdev@gmail.com>
2023-05-08 12:40:50 -0400

    remove am/pm for column sorting

--- resources/views/order.blade.php
+++ resources/views/order.blade.php
@@ -288,10 +288,10 @@
                                                     <td nowrap><small>{{
                                                     \Carbon\Carbon::parse
                                                     ($order->enteredTime)
-                                                    ->setTimezone('America/New_York')->format('Y-m-d g:i:s A') }}</small></td>
+                                                    ->setTimezone('America/New_York')->format('Y-m-d g:i:s') }}</small></td>
 {{--                                                    <td>{{ \Carbon\Carbon::parse($order->closeTime)->format('g:i:s a') }}</td>--}}
                                                     <td nowrap><small>{{
-                                                    \Carbon\Carbon::parse($order->closeTime)->setTimezone('America/New_York')->format('Y-m-d g:i:s A') }}</small></td>
+                                                    \Carbon\Carbon::parse($order->closeTime)->setTimezone('America/New_York')->format('Y-m-d g:i:s') }}</small></td>
 {{--                                                    <td>{{ \Carbon\Carbon::parse($order->closeTime)->toDateTimeString() }}</td>--}}
 
                                                     <td>{{ gmdate('H:i:s', \Carbon\Carbon::parse($order->closeTime)->diffInSeconds($order->enteredTime)) }}</td>
