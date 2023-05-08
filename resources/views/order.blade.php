157bc5f63a5dfc2d8229fd930a307428b4fff63e
PuReWebDev <purewebdev@gmail.com>
2023-05-07 22:28:46 -0400

    formatting date time columns

--- resources/views/order.blade.php
+++ resources/views/order.blade.php
@@ -282,13 +282,13 @@
                                                         @endisset
                                                     </td>
                                                     <td style="text-align:center">{{ $order->quantity }}</td>
-                                                    <td nowrap>{{
+                                                    <td nowrap><small>{{
                                                     \Carbon\Carbon::parse
                                                     ($order->enteredTime)
-                                                    ->setTimezone('America/New_York')->format('Y-m-d g:i:s A') }}</td>
+                                                    ->setTimezone('America/New_York')->format('Y-m-d g:i:s A') }}</small></td>
 {{--                                                    <td>{{ \Carbon\Carbon::parse($order->closeTime)->format('g:i:s a') }}</td>--}}
-                                                    <td nowrap>{{
-                                                    \Carbon\Carbon::parse($order->closeTime)->setTimezone('America/New_York')->format('Y-m-d g:i:s A') }}</td>
+                                                    <td nowrap><small>{{
+                                                    \Carbon\Carbon::parse($order->closeTime)->setTimezone('America/New_York')->format('Y-m-d g:i:s A') }}</small></td>
 {{--                                                    <td>{{ \Carbon\Carbon::parse($order->closeTime)->toDateTimeString() }}</td>--}}
 
                                                     <td>{{ gmdate('H:i:s', \Carbon\Carbon::parse($order->closeTime)->diffInSeconds($order->enteredTime)) }}</td>
