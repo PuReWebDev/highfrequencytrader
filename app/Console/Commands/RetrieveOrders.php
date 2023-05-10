5df017cd016988f5898ca38f0a11b611ea3b947c
PuReWebDev <purewebdev@gmail.com>
2023-05-10 14:17:08 -0400

    the break when needed

--- app/Console/Commands/RetrieveOrders.php
+++ app/Console/Commands/RetrieveOrders.php
@@ -107,10 +107,10 @@ class RetrieveOrders extends Command
                     $this->info('Individual Order Retrieved and Updated: '.
                         $order['orderId']. ' And '. $count-- .' remaining');
                     usleep(5000000);
-                    if ($count === 1) {
-                        break;
-                    }
+
                 }
+
+                break;
             }
 
             if ($status === 'FILLED' || empty($status)) {
