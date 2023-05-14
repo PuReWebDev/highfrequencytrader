93a52a886c01f9ca10a8e28ce4d1a41ae66d3867
PuReWebDev <purewebdev@gmail.com>
2023-05-14 18:33:27 -0400

    new Strategy model

---
+++ app/Models/Strategy.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Strategy extends Model
+{
+    use HasFactory;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'strategies';
+
+    protected $fillable = [
+        'user_id',
+        'strategy_name',
+        'enabled',
+        'trade_quantity',
+        'number_of_trades',
+        'running_counts',
+        'max_stock_price',
+        'stops_allowed',
+        'change_quantity_after_stops',
+        'quantity_after_stop',
+        'stop_price',
+        'limit_price',
+        'limit_price_offset',
+        'high_price_buffer',
+        'profit',
+        'symbols',
+    ];
+
+    /**
+     * Get the user that owns the phone.
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
