b2541ae782cb33de8220e571efec68cad22924c9
PuReWebDev <purewebdev@gmail.com>
2023-05-14 23:45:14 -0400

    add a link to the create strategy page

--- resources/views/strategy.blade.php
+++ resources/views/strategy.blade.php
@@ -9,6 +9,8 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
+                    <a href="/strategy/create">Create A New Trade
+                        Strategy</a><br />
                     @if (count($strategies) >= 1)
                     <div class="table-responsive">
                         <table class="table table-striped caption-top"
