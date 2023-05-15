624b4c21a39420300fbc1e3655ac8a6ff35d30e2
PuReWebDev <purewebdev@gmail.com>
2023-05-14 20:56:36 -0400

    adjusted

--- resources/views/strategy-create.blade.php
+++ resources/views/strategy-create.blade.php
@@ -214,55 +214,52 @@
 
     <script type="text/javascript">
         // ------------step-wizard-------------
-        $(document).ready(function () {
-            $(function() {
-                // SmartWizard initialize
-                const myModal = new bootstrap.Modal(document.getElementById('confirmModal'));
+        const myModal = new bootstrap.Modal(document.getElementById('confirmModal'));
 
 
-                function onCancel() {
-                    // Reset wizard
-                    $('#smartwizard').smartWizard("reset");
+        function onCancel() {
+            // Reset wizard
+            $('#smartwizard').smartWizard("reset");
 
-                    // Reset form
-                    document.getElementById("form-1").reset();
-                    document.getElementById("form-2").reset();
-                    document.getElementById("form-3").reset();
-                    document.getElementById("form-4").reset();
+            // Reset form
+            document.getElementById("form-1").reset();
+            document.getElementById("form-2").reset();
+            document.getElementById("form-3").reset();
+            document.getElementById("form-4").reset();
+        }
+
+        function onConfirm() {
+            let form = document.getElementById('form-4');
+            if (form) {
+                if (!form.checkValidity()) {
+                    form.classList.add('was-validated');
+                    $('#smartwizard').smartWizard("setState", [3], 'error');
+                    $("#smartwizard").smartWizard('fixHeight');
+                    return false;
                 }
 
-                function onConfirm() {
-                    let form = document.getElementById('form-4');
-                    if (form) {
-                        if (!form.checkValidity()) {
-                            form.classList.add('was-validated');
-                            $('#smartwizard').smartWizard("setState", [3], 'error');
-                            $("#smartwizard").smartWizard('fixHeight');
-                            return false;
-                        }
+                myModal.show();
+            }
+        }
 
-                        myModal.show();
-                    }
-                }
+        function closeModal() {
+            // Reset wizard
+            $('#smartwizard').smartWizard("reset");
 
-                function closeModal() {
-                    // Reset wizard
-                    $('#smartwizard').smartWizard("reset");
+            // Reset form
+            document.getElementById("form-1").reset();
+            document.getElementById("form-2").reset();
+            document.getElementById("form-3").reset();
+            document.getElementById("form-4").reset();
 
-                    // Reset form
-                    document.getElementById("form-1").reset();
-                    document.getElementById("form-2").reset();
-                    document.getElementById("form-3").reset();
-                    document.getElementById("form-4").reset();
+            myModal.hide();
+        }
 
-                    myModal.hide();
-                }
-
-                function showConfirm() {
-                    const name = $('#first-name').val() + ' ' + $('#last-name').val();
-                    const products = $('#sel-products').val();
-                    const shipping = $('#address').val() + ' ' + $('#state').val() + ' ' + $('#zip').val();
-                    let html = `<h4 class="mb-3-">Customer Details</h4>
+        function showConfirm() {
+            const name = $('#first-name').val() + ' ' + $('#last-name').val();
+            const products = $('#sel-products').val();
+            const shipping = $('#address').val() + ' ' + $('#state').val() + ' ' + $('#zip').val();
+            let html = `<h4 class="mb-3-">Customer Details</h4>
                   <hr class="my-2">
                   <div class="row g-3 align-items-center">
                     <div class="col-auto">
@@ -288,98 +285,96 @@
                       <span class="form-text-">${shipping}</span>
                     </div>
                   </div>`;
-                    $("#order-details").html(html);
-                    $('#smartwizard').smartWizard("fixHeight");
+            $("#order-details").html(html);
+            $('#smartwizard').smartWizard("fixHeight");
+        }
+
+        $(function() {
+            // Leave step event is used for validating the forms
+            $("#smartwizard").on("leaveStep", function(e, anchorObject, currentStepIdx, nextStepIdx, stepDirection) {
+                // Validate only on forward movement
+                if (stepDirection == 'forward') {
+                    let form = document.getElementById('form-' + (currentStepIdx + 1));
+                    if (form) {
+                        if (!form.checkValidity()) {
+                            form.classList.add('was-validated');
+                            $('#smartwizard').smartWizard("setState", [currentStepIdx], 'error');
+                            $("#smartwizard").smartWizard('fixHeight');
+                            return false;
+                        }
+                        $('#smartwizard').smartWizard("unsetState", [currentStepIdx], 'error');
+                    }
+                }
+            });
+
+            // Step show event
+            $("#smartwizard").on("showStep", function(e, anchorObject, stepIndex, stepDirection, stepPosition) {
+                $("#prev-btn").removeClass('disabled').prop('disabled', false);
+                $("#next-btn").removeClass('disabled').prop('disabled', false);
+                if(stepPosition === 'first') {
+                    $("#prev-btn").addClass('disabled').prop('disabled', true);
+                } else if(stepPosition === 'last') {
+                    $("#next-btn").addClass('disabled').prop('disabled', true);
+                } else {
+                    $("#prev-btn").removeClass('disabled').prop('disabled', false);
+                    $("#next-btn").removeClass('disabled').prop('disabled', false);
                 }
 
-                $(function() {
-                    // Leave step event is used for validating the forms
-                    $("#smartwizard").on("leaveStep", function(e, anchorObject, currentStepIdx, nextStepIdx, stepDirection) {
-                        // Validate only on forward movement
-                        if (stepDirection == 'forward') {
-                            let form = document.getElementById('form-' + (currentStepIdx + 1));
-                            if (form) {
-                                if (!form.checkValidity()) {
-                                    form.classList.add('was-validated');
-                                    $('#smartwizard').smartWizard("setState", [currentStepIdx], 'error');
-                                    $("#smartwizard").smartWizard('fixHeight');
-                                    return false;
-                                }
-                                $('#smartwizard').smartWizard("unsetState", [currentStepIdx], 'error');
-                            }
-                        }
-                    });
+                // Get step info from Smart Wizard
+                let stepInfo = $('#smartwizard').smartWizard("getStepInfo");
+                $("#sw-current-step").text(stepInfo.currentStep + 1);
+                $("#sw-total-step").text(stepInfo.totalSteps);
 
-                    // Step show event
-                    $("#smartwizard").on("showStep", function(e, anchorObject, stepIndex, stepDirection, stepPosition) {
-                        $("#prev-btn").removeClass('disabled').prop('disabled', false);
-                        $("#next-btn").removeClass('disabled').prop('disabled', false);
-                        if(stepPosition === 'first') {
-                            $("#prev-btn").addClass('disabled').prop('disabled', true);
-                        } else if(stepPosition === 'last') {
-                            $("#next-btn").addClass('disabled').prop('disabled', true);
-                        } else {
-                            $("#prev-btn").removeClass('disabled').prop('disabled', false);
-                            $("#next-btn").removeClass('disabled').prop('disabled', false);
-                        }
+                if (stepPosition == 'last') {
+                    showConfirm();
+                    $("#btnFinish").prop('disabled', false);
+                } else {
+                    $("#btnFinish").prop('disabled', true);
+                }
 
-                        // Get step info from Smart Wizard
-                        let stepInfo = $('#smartwizard').smartWizard("getStepInfo");
-                        $("#sw-current-step").text(stepInfo.currentStep + 1);
-                        $("#sw-total-step").text(stepInfo.totalSteps);
-
-                        if (stepPosition == 'last') {
-                            showConfirm();
-                            $("#btnFinish").prop('disabled', false);
-                        } else {
-                            $("#btnFinish").prop('disabled', true);
-                        }
-
-                        // Focus first name
-                        if (stepIndex == 1) {
-                            setTimeout(() => {
-                                $('#first-name').focus();
-                            }, 0);
-                        }
-                    });
-
-                    // Smart Wizard
-                    $('#smartwizard').smartWizard({
-                        selected: 0,
-                        // autoAdjustHeight: false,
-                        theme: 'arrows', // basic, arrows, square, round, dots
-                        transition: {
-                            animation:'none'
-                        },
-                        toolbar: {
-                            showNextButton: true, // show/hide a Next button
-                            showPreviousButton: true, // show/hide a Previous button
-                            position: 'bottom', // none/ top/ both bottom
-                            extraHtml: `<button class="btn btn-success" id="btnFinish" disabled onclick="onConfirm()">Complete Order</button>
-                              <button class="btn btn-danger" id="btnCancel" onclick="onCancel()">Cancel</button>`
-                        },
-                        anchor: {
-                            enableNavigation: true, // Enable/Disable anchor navigation
-                            enableNavigationAlways: false, // Activates all anchors clickable always
-                            enableDoneState: true, // Add done state on visited steps
-                            markPreviousStepsAsDone: true, // When a step selected by url hash, all previous steps are marked done
-                            unDoneOnBackNavigation: true, // While navigate back, done state will be cleared
-                            enableDoneStateNavigation: true // Enable/Disable the done state navigation
-                        },
-                    });
-
-                    $("#state_selector").on("change", function() {
-                        $('#smartwizard').smartWizard("setState", [$('#step_to_style').val()], $(this).val(), !$('#is_reset').prop("checked"));
-                        return true;
-                    });
-
-                    $("#style_selector").on("change", function() {
-                        $('#smartwizard').smartWizard("setStyle", [$('#step_to_style').val()], $(this).val(), !$('#is_reset').prop("checked"));
-                        return true;
-                    });
-
-                });
+                // Focus first name
+                if (stepIndex == 1) {
+                    setTimeout(() => {
+                        $('#first-name').focus();
+                    }, 0);
+                }
             });
+
+            // Smart Wizard
+            $('#smartwizard').smartWizard({
+                selected: 0,
+                // autoAdjustHeight: false,
+                theme: 'arrows', // basic, arrows, square, round, dots
+                transition: {
+                    animation:'none'
+                },
+                toolbar: {
+                    showNextButton: true, // show/hide a Next button
+                    showPreviousButton: true, // show/hide a Previous button
+                    position: 'bottom', // none/ top/ both bottom
+                    extraHtml: `<button class="btn btn-success" id="btnFinish" disabled onclick="onConfirm()">Complete Order</button>
+                              <button class="btn btn-danger" id="btnCancel" onclick="onCancel()">Cancel</button>`
+                },
+                anchor: {
+                    enableNavigation: true, // Enable/Disable anchor navigation
+                    enableNavigationAlways: false, // Activates all anchors clickable always
+                    enableDoneState: true, // Add done state on visited steps
+                    markPreviousStepsAsDone: true, // When a step selected by url hash, all previous steps are marked done
+                    unDoneOnBackNavigation: true, // While navigate back, done state will be cleared
+                    enableDoneStateNavigation: true // Enable/Disable the done state navigation
+                },
+            });
+
+            $("#state_selector").on("change", function() {
+                $('#smartwizard').smartWizard("setState", [$('#step_to_style').val()], $(this).val(), !$('#is_reset').prop("checked"));
+                return true;
+            });
+
+            $("#style_selector").on("change", function() {
+                $('#smartwizard').smartWizard("setStyle", [$('#step_to_style').val()], $(this).val(), !$('#is_reset').prop("checked"));
+                return true;
+            });
+
         });
     </script>
 
