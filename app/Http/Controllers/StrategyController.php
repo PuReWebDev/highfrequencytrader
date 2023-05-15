83bdbc2afcf3579385ad4f0063a5a63a16fc21de
PuReWebDev <purewebdev@gmail.com>
2023-05-15 12:22:05 -0400

    some data handling

--- app/Http/Controllers/StrategyController.php
+++ app/Http/Controllers/StrategyController.php
@@ -44,7 +44,38 @@ class StrategyController extends Controller
      */
     public function store(Request $request)
     {
-        Log::debug('New Strategy Posted', $request->toArray());
+        $strategy_name = $request->input('strategy_name');
+        $enabled = $request->input('enabled');
+        $trade_quantity = $request->input('trade_quantity');
+        $number_of_trades = $request->input('number_of_trades');
+        $running_counts = $request->input('running_counts');
+        $max_stock_price = $request->input('max_stock_price');
+        $max_stops_allowed = $request->input('max_stops_allowed');
+        $change_quantity_after_stops = $request->input('change_quantity_after_stops');
+        $quantity_after_stop = $request->input('quantity_after_stop');
+        $stop_price = $request->input('stop_price');
+        $limit_price = $request->input('limit_price');
+        $limit_price_offset = $request->input('limit_price_offset');
+        $high_price_buffer= $request->input('high_price_buffer');
+        $profit = $request->input('profit');
+
+        $data = [
+            'strategy_name' => $strategy_name,
+            'enabled' => $enabled,
+            'trade_quantity' => $trade_quantity,
+            'number_of_trades' => $number_of_trades,
+            'running_counts' => $running_counts,
+            'max_stock_price' => $max_stock_price,
+            'max_stops_allowed' => $max_stops_allowed,
+            'change_quantity_after_stops' => $change_quantity_after_stops,
+            'quantity_after_stop' => $quantity_after_stop,
+            'stop_price' => $stop_price,
+            'limit_price' => $limit_price,
+            'limit_price_offset' => $limit_price_offset,
+            'high_price_buffer' => $high_price_buffer,
+            'profit' => $profit,
+        ];
+        Log::debug('New Strategy Posted', $data);
         return response()->json([
             'success' => true,
             'data' => $request->toArray(),
