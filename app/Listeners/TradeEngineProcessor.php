b7506d7a9faed233b909af965a0af1b215018372
PuReWebDev <purewebdev@gmail.com>
2023-05-17 12:27:50 -0400

    increase every hour

--- app/Listeners/TradeEngineProcessor.php
+++ app/Listeners/TradeEngineProcessor.php
@@ -109,7 +109,7 @@ class TradeEngineProcessor
 
         foreach ($this->tradeSymbols as $tradeSymbol) {
             // Set Some Default Values
-            $this->shareQuantityPerTrade[$tradeSymbol] = 4;
+            $this->shareQuantityPerTrade[$tradeSymbol] = 5;
             $tradeHalted[$tradeSymbol] = false;
 
 //            $this->shareQuantityPerTrade['MSFT'] = 10;
