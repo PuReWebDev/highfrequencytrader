2b7b6a9275d2e39100da5225b87d92b7e12c3473
PuReWebDev <purewebdev@gmail.com>
2023-05-17 10:21:50 -0400

    don't fetch the price right now

--- app/Console/Commands/RetrieveOrders.php
+++ app/Console/Commands/RetrieveOrders.php
@@ -111,9 +111,9 @@ class RetrieveOrders extends Command
                     $this->info('Individual Order Retrieved and Updated: '.
                         $order['orderId']. ' And '. $count-- .' remaining '.Carbon::now()->setTimezone('America/New_York')->format('Y-m-d g:i A'));
                     usleep(2500000);
-                    Log::info('Fetching Price History For '. $order['symbol']);
-                    TDAmeritrade::getPriceHistory($order['symbol']);
-                    usleep(2500000);
+//                    Log::info('Fetching Price History For '. $order['symbol']);
+//                    TDAmeritrade::getPriceHistory($order['symbol']);
+//                    usleep(2500000);
 
                 }
 
