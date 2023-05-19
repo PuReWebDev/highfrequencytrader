28bfea41962e57f718c78b5f8f7191563558d997
PuReWebDev <purewebdev@gmail.com>
2023-05-19 10:55:12 -0400

    dynamic increase

--- app/Listeners/TradeEngineProcessor.php
+++ app/Listeners/TradeEngineProcessor.php
@@ -142,7 +142,8 @@ class TradeEngineProcessor
                 $tradeHalted[$tradeSymbol] = true;
                 Log::info('Concurrency for Stopped Symbol Disabled '.$tradeSymbol);
             }
-            if ($runningCounts[$tradeSymbol] >= 2) {
+//            if ($runningCounts[$tradeSymbol] >= 2) {
+            if ($runningCounts[$tradeSymbol] >= self::quantityOverTime()) {
                 $this->shareQuantityPerTrade[$tradeSymbol] = 2;
                 $tradeHalted[$tradeSymbol] = true;
             }
