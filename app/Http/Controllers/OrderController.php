acdd2c77aa0c9ba9c9e7ccacf51091a50bf9fcf4
PuReWebDev <purewebdev@gmail.com>
2023-05-20 13:53:29 -0400

    adjusting

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -181,7 +181,7 @@ class OrderController extends Controller
             $stats = TDAmeritrade::extracted($filtered);
 
             $statistics[$symbol['symbol']] = [
-                'workingCount' => $stats['0']['0'],
+                'workingCount' => $stats->get('0'),
                 'filledCount' => $stats['1']['FILLED']?? 0,
                 'rejectedCount' =>$stats['2']['REJECTED']?? 0,
                 'cancelledCount' => $stats['3']['CANCELED']?? 0,
