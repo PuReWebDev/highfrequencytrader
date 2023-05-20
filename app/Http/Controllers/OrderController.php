6868a6832cc31a85a5e2335bf7ff977a263bf118
PuReWebDev <purewebdev@gmail.com>
2023-05-20 18:09:15 -0400

    debug

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -181,7 +181,7 @@ class OrderController extends Controller
             $stats = TDAmeritrade::extracted($filtered);
 
             $statistics[$symbol['symbol']] = [
-                'workingCount' => $stats['0'] instanceof Collection ? $stats['0']->get('0') : $stats['0'],
+                'workingCount' => $stats['0'] instanceof Collection ? $stats['0']->get(0, 'Nothing better to put here') : $stats['0'],
                 'filledCount' => $stats['1']['FILLED']?? 0,
                 'rejectedCount' =>$stats['2']['REJECTED']?? 0,
                 'cancelledCount' => $stats['3']['CANCELED']?? 0,
