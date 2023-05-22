fb8ad9d0ffb314ce50dc4ba3d7d70c87a6fa9a04
PuReWebDev <purewebdev@gmail.com>
2023-05-22 11:53:20 -0400

    update our stop

--- app/Listeners/TradeEngineProcessor.php
+++ app/Listeners/TradeEngineProcessor.php
@@ -212,7 +212,7 @@ class TradeEngineProcessor
                 OrderService::placeOtoOrder(
                     number_format($currentStockPrice, 2, '.', ''),
                     number_format($currentStockPrice + .05,2, '.', ''),
-                    number_format($currentStockPrice - 0.60, 2, '.', ''),
+                    number_format($currentStockPrice - 1.00, 2, '.', ''),
                     $quote->symbol, $this->shareQuantityPerTrade[$quote->symbol]);
 
                 $message = "Order placed: Buy ".number_format($currentStockPrice, 2, '.',
