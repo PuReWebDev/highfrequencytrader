d0f4482cfb14aae3b66a9dd5d15504096cca506f
GeorgieSantiago <milestoneartisan@gmail.com>
2023-06-02 23:22:32 -0400

    chore: remove unused imports and minor cleanup

--- app/Http/Controllers/Api/PriceHistoryApiController.php
+++ app/Http/Controllers/Api/PriceHistoryApiController.php
@@ -4,18 +4,10 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers\Api;
 
-use App\Models\Account;
-use App\Models\Balance;
-use App\Models\Order;
-use App\Models\Position;
-use App\Models\Token;
-use App\TDAmeritrade\Accounts;
 use App\TDAmeritrade\TDAmeritrade;
-use Carbon\Carbon;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\View;
 
 class PriceHistoryApiController extends Controller
 {
@@ -23,10 +15,10 @@ class PriceHistoryApiController extends Controller
      * Display the specified resource.
      *
      * @param Request $request
-     * @param Account $account
+     * @param string $symbol
      * @return \Illuminate\Http\JsonResponse
      */
-    public function show(Request $request, string $symbol)
+    public function show(Request $request, string $symbol) : JsonResponse
     {
         try {
             return $this->success(TDAmeritrade::getPriceHistory( 
